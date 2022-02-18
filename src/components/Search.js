2caca6b7cd6d3c5763031bb1f354daa561cf1ab3
ZsombiTech <horvathzsombor2005@gmail.com>
2022-02-18 18:18:00 +0100

    ..

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,23 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import "../styles/Search.css";
 
 export default function Search() {
+  const [name, setName] = useState("");
+
+  const inputhandler = (event) => {
+    setName(event.target.value);
+  };
+
   return (
     <Fragment>
       <div className="fullsbar">
-        <input type="text" id="search" placeholder="Username" />
-        <button>Search</button>
+        <input
+          type="text"
+          id="search"
+          placeholder="Username"
+          onChange={inputhandler}
+          value={name}
+        />
       </div>
     </Fragment>
   );
